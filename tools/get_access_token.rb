9a9638a77c4cd7287f19f1e49b0771c68c3df2fe
Takafumi ONAKA <takafumi.onaka@gmail.com>
2015-03-09 08:08:10 +0900

    omniauth-tumblr で行うようになったので不要

--- tools/get_access_token.rb
+++
@@ -1,7 +0,0 @@
-# rails runner tools/get_access_token.rb
-require "oauth"
-consumer = OAuth::Consumer.new(Global.app.consumer_key,
-                               Global.app.consumer_secret,
-                               site: "http://www.tumblr.com")
-request_token = consumer.get_request_token(exclude_callback: true)
-`open #{request_token.authorize_url}`
