576cb06e6e5d84527fe56ac70ccdb9248395ef22
Takafumi ONAKA <takafumi.onaka@gmail.com>
2020-10-26 06:22:22 +0900

    Update rails from v5.1.7 to v5.2.8.1

--- Gemfile
+++ Gemfile
@@ -1,14 +1,11 @@
 source "https://rubygems.org"
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-git_source(:github) do |repo_name|
-  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
-  "https://github.com/#{repo_name}.git"
-end
-
-gem "rails", "5.1.7"
+gem "rails", "5.2.8.1"
 
 gem "activerecord-simple_index_name"
 gem "carrierwave"
+gem "bootsnap", require: false
 gem "coffee-rails"
 gem "fastimage", require: false
 gem "global"
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -19,11 +20,12 @@ Bundler.require(*Rails.groups)
 module TumblrArchiver
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Don't generate system test files.
     config.generators.system_tests = nil
