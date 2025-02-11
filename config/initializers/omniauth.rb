aa725e35c21bb8372ae5facaa300b18d9bf789ef
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-02-11 22:55:25 +0900

    omniauth v2.0 からの POST 強制に対応
    1 provider しか提供していないので問題が無い

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  configure do |config|
+    config.allowed_request_methods = %i[get post]
+    config.silence_get_warning = true
+  end
   provider :tumblr, Global.app.consumer_key, Global.app.consumer_secret
 end
