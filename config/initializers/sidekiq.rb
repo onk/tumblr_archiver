026a4c248e56e3565d7514d7c206473c517e57e2
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-02-15 18:08:06 +0900

    redis-client は { host, port, db } を受け付ける

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,25 +1,13 @@
 require "sidekiq/web"
 require "sidekiq/cron/web"
 
-module RedisConnectionSetting
-  def self.to_url(config)
-    config = config.with_indifferent_access
-    "redis://#{config[:host]}:#{config[:port]}/#{config[:db]}"
-  end
-end
-
 Sidekiq.configure_server do |config|
-  config.redis = {
-    url: RedisConnectionSetting.to_url(Global.redis.queue.to_hash),
-  }
-
+  config.redis = Global.redis.queue.to_hash
   schedule_file = Rails.root.join("config/schedule.yml")
   Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(schedule_file))
 end
 
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
-  config.redis = {
-    url: RedisConnectionSetting.to_url(Global.redis.queue.to_hash),
-  }
+  config.redis = Global.redis.queue.to_hash
 end
