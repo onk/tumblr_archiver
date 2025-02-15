56c3d3c51ff6926494e8f04e1e3cf6d26c2f86ad
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-02-15 18:17:05 +0900

    Drop using global gem for Sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,16 @@
 require "sidekiq/web"
 require "sidekiq/cron/web"
 
+yaml = Rails.application.root.join("config/redis.yml")
+loaded_yaml = ActiveSupport::ConfigurationFile.parse(yaml)
+
 Sidekiq.configure_server do |config|
-  config.redis = Global.redis.queue.to_hash
+  config.redis = loaded_yaml[Rails.env]["queue"]
   schedule_file = Rails.root.join("config/schedule.yml")
   Sidekiq::Cron::Job.load_from_hash!(YAML.load_file(schedule_file))
 end
 
 # When in Unicorn, this block needs to go in unicorn's `after_fork` callback:
 Sidekiq.configure_client do |config|
-  config.redis = Global.redis.queue.to_hash
+  config.redis = loaded_yaml[Rails.env]["queue"]
 end
