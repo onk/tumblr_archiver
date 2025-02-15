5ab57044d2cc5d30fe1704c64444a6143826effd
Takafumi ONAKA <takafumi.onaka@gmail.com>
2025-02-16 01:08:52 +0900

    consumer_key, consumer_secret を credentials に入れる

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     config.allowed_request_methods = %i[get post]
     config.silence_get_warning = true
   end
-  provider :tumblr, Global.app.consumer_key, Global.app.consumer_secret
+  provider :tumblr, Rails.application.credentials.consumer_key, Rails.application.credentials.consumer_secret
 end
